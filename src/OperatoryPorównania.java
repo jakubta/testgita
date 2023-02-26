f6c8edc97b8e279d3826868aa79f9d4e12132224
jakubta <jakub.tasinski@gmail.com>
2023-02-26 22:08:02 +0100

    add OperatoryPorównania

---
+++ src/OperatoryPorównania.java
@@ -0,0 +1,16 @@
+public class OperatoryPorównania {
+
+    public static void main(String[] args) {
+        int firstNumber = 4;
+        int secondNumber = 6;
+
+        boolean result = secondNumber > firstNumber;
+        System.out.println(result);
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber < secondNumber);
+        System.out.println(firstNumber >= secondNumber);
+        System.out.println(firstNumber <= secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber != secondNumber);
+    }
+}
