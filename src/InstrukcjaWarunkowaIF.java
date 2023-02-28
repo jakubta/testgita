078fbb7c4fa39d31f89d1569fd67d05d1dc75f86
jakubta <jakub.tasinski@gmail.com>
2023-02-28 11:27:52 +0100

    add operatory logiczne i Instrukcje Warunkowe If, ElseIf

---
+++ src/InstrukcjaWarunkowaIF.java
@@ -0,0 +1,14 @@
+public class InstrukcjaWarunkowaIF {
+
+    public static void main(String[] args) {
+
+        float dzielna = 4.0F;
+        float dzielnik = 0.0F;
+
+        if (dzielnik != 0) {
+            System.out.println("Wynik dzilenia to: " + dzielna/dzielnik);
+        } else {
+            System.out.println("Proszę podaj inna liczbę");
+        }
+    }
+}
